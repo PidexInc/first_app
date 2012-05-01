c95baa09609693453b9b029eb241ddfd14d3bc57
PidexInc <request@pidex.ca>
2012-05-01 18:04:02 +0530

    Initial commit for test first_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '01f13f1425e347c90beb7fc00846a26190ff456e036e3b2d72a2c022b1dd819e34db9bc29bc2998ee906bebd0afbca8e484d9ff5311379aae447859c38de8486'
